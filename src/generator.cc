2352c86b2b73f61591f63cff338ea78b350aeade
Dung Phan <brianp.dung@utexas.edu>
2022-09-26 12:27:32 -0500

    Making X-ray beam flat/circle profile.

--- src/generator.cc
+++ src/generator.cc
@@ -1,33 +1,32 @@
 #include "generator.hh"
 
 generator::generator() {
-  fGeneralParticleSource = new G4GeneralParticleSource();
+    fGeneralParticleSource = new G4GeneralParticleSource();
 
-  G4SingleParticleSource *sps = fGeneralParticleSource->GetCurrentSource();
-  G4ParticleDefinition *particle = G4ParticleTable::GetParticleTable()->FindParticle("neutron");
+    G4SingleParticleSource *sps = fGeneralParticleSource->GetCurrentSource();
+    G4ParticleDefinition *particle = G4ParticleTable::GetParticleTable()->FindParticle("gamma");
 
-  sps->SetParticleDefinition(particle);
-  sps->SetNumberOfParticles(10000);
+    sps->SetParticleDefinition(particle);
+    sps->SetNumberOfParticles(1);
 
-  sps->GetPosDist()->SetPosDisType("Point"); // Point, Beam, Plane, Surface, Volume
-  sps->GetPosDist()->SetCentreCoords(G4ThreeVector(0., 0., 0.));
-  sps->GetPosDist()->ConfineSourceToVolume("NULL");
+    sps->GetPosDist()->SetPosDisType("Plane"); // Point, Beam, Plane, Surface, Volume
+    sps->GetPosDist()->SetPosDisShape("Circle");
+    sps->GetPosDist()->SetRadius(1 * cm);
+    sps->GetPosDist()->SetCentreCoords(G4ThreeVector(0., 0., 2 * cm));
+    sps->GetPosDist()->ConfineSourceToVolume("NULL");
 
-  sps->GetAngDist()->SetAngDistType("iso"); // Isotropic, Cosine-law, Beam, User-defined
+    sps->GetAngDist()->SetAngDistType("planar"); // Isotropic, Cosine-law, Beam, User-defined
 
-  sps->GetAngDist()->SetMinTheta(0. * rad);
-  sps->GetAngDist()->SetMaxTheta(TMath::Pi() * rad);
-  sps->GetAngDist()->SetMinPhi(0 * rad);
-  sps->GetAngDist()->SetMaxPhi(TMath::Pi() * 2. * rad);
-
-  sps->GetEneDist()->SetEnergyDisType("Exp"); // Mono, Lin, Pow, Exp, Gaus, Brem, BBody, Cdg (cosmic diffuse gamma), User, Arb, Epn (energy per nucleon)
-  sps->GetEneDist()->SetMonoEnergy(4.434 * MeV);                                              
+    sps->GetEneDist()->SetEnergyDisType("Bbody"); // Mono, Lin, Pow, Exp, Gaus, Brem, BBody, Cdg (cosmic diffuse gamma), User, Arb, Epn (energy per nucleon)
+    sps->GetEneDist()->SetEmin(1*keV);
+    sps->GetEneDist()->SetEmax(20*keV);
+    sps->GetEneDist()->SetTemp(6E7);
 }
 
 generator::~generator() {
-  delete fGeneralParticleSource;
+    delete fGeneralParticleSource;
 }
 
 void generator::GeneratePrimaries(G4Event *anEvent) {
-  fGeneralParticleSource->GeneratePrimaryVertex(anEvent);
+    fGeneralParticleSource->GeneratePrimaryVertex(anEvent);
 }
